30002ba0d01316342fa029ccdefad01a11a895ee
jordanmefujie <mjordanlonpo@gmail.com>
2023-04-21 20:00:41 -0700

    A program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -175,7 +175,7 @@ void print_abi(unsigned char *e_ident)
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
-	       	e_type >>= 8;
+		e_type >>= 8;
 	printf("  Type:                              ");
 	switch (e_type)
 	{
