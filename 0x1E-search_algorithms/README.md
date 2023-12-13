721563761daef6bf265067c771865ba7361ddba6
jordanmefujie <mjordanlonpo@gmail.com>
2023-12-13 08:09:29 -0800

    My first commit

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,37 @@
+This repository contains implementations of various search algorithms in the C programming language. These algorithms are essential tools in computer science and are used to efficiently find specific elements in datasets. The project covers both basic and advanced search algorithms.
+Table of Contents
+Search Algorithms
+Table of Contents
+Getting Started
+Prerequisites
+Installation
+Project Structure
+Usage
+Linear Search
+Binary Search
+Jump Search
+Interpolation Search
+Exponential Search
+Advanced Binary Search
+Jump Search in a Singly Linked List
+Linear Search in a Skip List
+Contributing
+License
+Acknowledgments
+
+The project is organized as follows:
+0-linear.c: Linear search implementation.
+1-binary.c: Binary search implementation.
+2-O: Time complexity analysis for linear search.
+3-O: Space complexity analysis for iterative linear search.
+4-O: Time complexity analysis for binary search.
+5-O: Space complexity analysis for binary search.
+6-O: Space complexity analysis for the allocate_map function.
+100-jump.c: Jump search implementation.
+101-O: Average-case time complexity analysis for jump search.
+102-interpolation.c: Interpolation search implementation.
+103-exponential.c: Exponential search implementation.
+104-advanced_binary.c: Advanced binary search implementation.
+105-jump_list.c: Jump search in a singly linked list implementation.
+106-linear_skip.c: Linear search in a skip list implementation.
+listint/: Additional files for singly linked list management.
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,74 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+
+/**
+ * struct listint_s - Singly linked list node structure.
+ *
+ * @n: Integer stored at the node.
+ * @index: Index of the node in the list.
+ * @next: Pointer to the next node.
+ *
+ * Description:
+ * This structure defines a node for a singly linked list. It contains an
+ * integer value, an index indicating its position in the list, and a
+ * pointer to the next node in the list.
+ */
+typedef struct listint_s
+{
+    int n;               /* Integer stored at the node. */
+    size_t index;        /* Index of the node in the list. */
+    struct listint_s *next; /* Pointer to the next node. */
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list node structure with an express lane.
+ *
+ * @n: Integer stored at the node.
+ * @index: Index of the node in the list.
+ * @next: Pointer to the next node.
+ * @express: Pointer to the next node in the express lane.
+ *
+ * Description:
+ * This structure defines a node for a singly linked list with an express lane.
+ * It contains an integer value, an index indicating its position in the list,
+ * a pointer to the next node in the regular lane, and a pointer to the next
+ * node in the express lane.
+ */
+typedef struct skiplist_s
+{
+    int n;               /* Integer stored at the node. */
+    size_t index;        /* Index of the node in the list. */
+    struct skiplist_s *next;    /* Pointer to the next node. */
+    struct skiplist_s *express; /* Pointer to the next node in the express lane. */
+} skiplist_t;
+
+/* Function prototypes for various search algorithms */
+
+int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+/* Function prototypes for list-related functions */
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* Helper functions for working with lists */
+
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+
+void free_skiplist(skiplist_t *list);
+
+#endif /* _SEARCH_ALGOS_H_ */
